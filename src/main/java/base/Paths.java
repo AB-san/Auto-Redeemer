de298db65630baa572f0096090ac10b575c6cd1f
ABsan <ansuman.behera@moveinsync.com>
2023-11-17 13:50:08 +0530

    Bug Fixes

--- src/main/java/base/Paths.java
+++ src/main/java/base/Paths.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class Paths {
     public static final String DEFAULT_DIR = System.getProperty("user.dir");
     public static String CLS_FARMING_PROPERTIES = DEFAULT_DIR + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "clsFarming.properties";
-//    public static String USER_PROPERTIES = DEFAULT_DIR + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + System.getProperty("propertyFileName");
-public static String USER_PROPERTIES = DEFAULT_DIR + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "ansuman.properties";
+    public static String USER_PROPERTIES = DEFAULT_DIR + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + System.getProperty("propertyFileName");
+//    to debug uncomment the following line and comment the above line
+//    public static String USER_PROPERTIES = DEFAULT_DIR + File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "ansuman.properties";
 }
--- src/main/java/pages/WithdrawPage.java
+++ src/main/java/pages/WithdrawPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import base.BaseActions;
 import base.Paths;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import utils.Reader;
 
+@Slf4j
 public class WithdrawPage extends BaseActions {
     public WebDriver webDriver;
     public WithdrawPage(WebDriver webDriver){
@@ -34,18 +36,23 @@ public class WithdrawPage extends BaseActions {
     @FindBy(xpath = "//div[contains(text(),'successful')]")
     public WebElement successPopup;
 
-    public String getTotalBalance(){
+    public int getTotalBalance(){
         String balance = totalBalance.getText();
-        return balance.substring(0, balance.indexOf("Rs"));
+        //only integer value will can be sent in the field.
+        double amt = Double.parseDouble(balance.substring(0, balance.indexOf("Rs")));
+        return (int)amt;
     }
 
     public void withdrawTotalBalance(){
-        withdrawAmtInputField.sendKeys("1000");
+        withdrawAmtInputField.sendKeys(String.valueOf(getTotalBalance()));
         waitFor700MilliSec();
         withdrawPwdInputField.sendKeys(Reader.getProperty("LOGIN_WITHDRAW_PASSWORD"));
         waitFor700MilliSec();
         click(withdrawButton);
-        Assert.assertTrue(checkIfElementIsDisplayed(successPopup),"Withdraw unsuccessful");
+        if(!checkIfElementIsDisplayed(successPopup)){
+            log.info("Withdraw unsuccessful");
+        }
+        waitFor300MilliSec();
         click(okayPopup);
     }
 
